8a9597c98d40a98c50efb388342515f65da852e9
Hilmi AM <imiia75775@proton.me>
2024-06-27 06:41:15 +0700

    aspect

---
+++ src/main/java/franxx/code/spring/aop/aspect/LogAspect.java
@@ -0,0 +1,9 @@
+package franxx.code.spring.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+}
