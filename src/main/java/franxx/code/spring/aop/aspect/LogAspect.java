4edf6e24baf76f9a6cc6e8707337c4182af69c4e
Hilmi AM <imiia75775@proton.me>
2024-06-27 06:44:36 +0700

    pointcut

--- src/main/java/franxx/code/spring/aop/aspect/LogAspect.java
+++ src/main/java/franxx/code/spring/aop/aspect/LogAspect.java
@@ -1,9 +1,15 @@
 package franxx.code.spring.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
+
+    @Pointcut()
+    public void helloServiceMethod() {
+
+    }
 }
