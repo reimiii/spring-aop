56bdf37fdb65d04b9aca268a4fba4b2ba111c019
Hilmi AM <imiia75775@proton.me>
2024-06-27 06:55:55 +0700

    advice

--- src/main/java/franxx/code/spring/aop/aspect/LogAspect.java
+++ src/main/java/franxx/code/spring/aop/aspect/LogAspect.java
@@ -1,15 +1,27 @@
 package franxx.code.spring.aop.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LogAspect {
 
     @Pointcut("target(franxx.code.spring.aop.service.HelloService)")
     public void helloServiceMethod() {
+    }
 
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod() {
+        log.info("before HelloService method");
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod2() {
+        log.info("before HelloService method again");
     }
 }
